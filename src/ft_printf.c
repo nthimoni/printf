60bedaeb18357d96c0cf7ee14478c8c8a12c1f1c
Nicolas THIMONIER <nthimoni@e2r7p9.clusters.42paris.fr>
2021-12-06 19:32:39 +0100

    ok

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 17:16:14 by nthimoni          #+#    #+#             */
-/*   Updated: 2021/12/06 17:30:56 by nthimoni         ###   ########.fr       */
+/*   Updated: 2021/12/06 18:00:40 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 int	ft_printf(const char *s, ...)
 {
 	char	*flag_pos;
+	va_list ap;
 
+	va_start(ap, s);
 	flag_pos = ft_strchr(s, '%');
 	while (flag_pos)
 	{
@@ -28,5 +30,6 @@ int	ft_printf(const char *s, ...)
 	}
 	write_buf(s, ft_strlen(s));
 	write_buf(s, 0);
+	va_end(ap);
 	return (0);
 }
