9ec7d508cbd5b42cdbba59bb3f8eb6cf8f7b0bfa
Nicolas THIMONIER <nthimoni@e2r8p11.clusters.42paris.fr>
2021-12-08 23:15:23 +0100

    CAPART

--- main.c
+++ main.c
@@ -5,5 +5,16 @@
 
 int main()
 {
-	ft_printf("%+2d", 1);
+	ft_printf("%04d", -14);
 }
+/*
+# include <limits.h>
+#include "include/ft_printf.h"
+int main()
+{
+    int ret = ft_printf(" % d ", LONG_MIN);
+    int ret2 =   printf(" % ld ", LONG_MIN);
+    printf("\n\nret val: %d vs %d", ret, ret2);
+    return 0;
+}
+*/
--- src/print_di.c
+++ src/print_di.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 17:03:31 by nthimoni          #+#    #+#             */
-/*   Updated: 2021/12/08 20:02:26 by nthimoni         ###   ########.fr       */
+/*   Updated: 2021/12/08 23:06:28 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,52 @@
 
 #include <stdio.h>
 
+void reg_dot(t_flags *flags, size_t len)
+{
+	if (flags->dot < (int)len)
+		flags->dot = len;
+}
+
+void	print_before(t_flags *flags, int a, unsigned int len, int sign)
+{
+	if (!flags->minus && !flags->zero && flags->size != -1)
+		print_n_char(' ', flags->size - flags->dot - sign, 0);
+	if (a < 0)
+		write_buf("-", 1);
+	else if (flags->plus)
+		write_buf("+", 1);
+	else if (flags->space)
+		write_buf(" ", 1);
+	if (!flags->minus && flags->zero && flags->size != -1)
+		print_n_char('0', flags->size - flags->dot - sign, 0);
+	print_n_char('0', flags->dot - len, 0);
+}
+
+void	print_di(int a, t_flags *flags)
+{
+	unsigned int	u;
+	unsigned int	len;
+	int				sign;
+
+	if (a < 0)
+		u = -a;
+	else
+		u = a;
+	len = ui_len(u, 10);
+	sign = 0;
+	if (a < 0 || flags->plus || flags->space)
+		sign = 1;
+	reg_dot(flags, len);
+	print_before(flags, a, len, sign);
+	print_ui_base(u, BASE_10);
+	if (flags->minus)
+		print_n_char(' ', flags->size - flags->dot - sign, 0);
+}
+
+
+
+
+/*
 void reg_dot(t_flags *flags, size_t len)
 {
 	if (flags->dot == -1)
@@ -36,7 +82,7 @@ void	print_before(t_flags *flags, int a, unsigned int len)
 	if (!flags->minus && flags->zero)
 		print_n_char('0', flags->size - flags->dot, 0);
 	if ((a < 0 || flags->space || flags->plus) 
-		&& !flags->minus && !flags->zero && flags->dot != (int)len)
+		&& !flags->minus && !flags->zero && flags->dot != (int)len - 1)
 		print_n_char('0', flags->dot - len + 1, 0);
 	if (!(a < 0) && !flags->space && !flags->plus && flags->dot != (int)len)
 		print_n_char('0', flags->dot - len, 0);
@@ -60,7 +106,7 @@ void	print_di(int a, t_flags *flags)
 	if (flags->minus)
 		print_n_char(' ', flags->size - flags->dot, 0);
 }
-
+*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
